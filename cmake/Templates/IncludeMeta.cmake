63a5d2d275033437a5f595315c2baf23bbeb851e
Pedro Oliveira <pedro.oliveira@vrviu.com>
2023-02-02 10:27:10 +0900

    Updates Meta HEADER file template

--- cmake/Templates/IncludeMeta.cmake
+++ cmake/Templates/IncludeMeta.cmake
@@ -1,5 +1,11 @@
 include_guard(GLOBAL)
 
+set(THIS_DIR ${CMAKE_CURRENT_LIST_DIR})
 function(configure_meta_header_file)
-  configure_file("Meta.h.in" "${CMAKE_BINARY_DIR}/Meta/Meta.h" ESCAPE_QUOTES)
+  string(TOLOWER COMPANY_NAME COMPANY_NAME_LOWER_CASE)
+  string(TOLOWER PROJECT_NAME PROJECT_NAME_LOWER_CASE)
+  configure_file(
+    "${THIS_DIR}/Meta.h.in" "${CMAKE_BINARY_DIR}/${COMPANY_NAME}/${PROJECT_NAME}/Meta.h"
+    ESCAPE_QUOTES
+  )
 endfunction()
