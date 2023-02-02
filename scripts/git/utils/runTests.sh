12ff7f90177dd1495138a07a49a3d57e9558dd87
Pedro Oliveira <pedro.oliveira@vrviu.com>
2023-02-02 11:13:43 +0900

    Updates git hooks and script utils

---
+++ scripts/git/utils/runTests.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# ============================================================================
+# Script setup
+# ============================================================================
+
+# navigate to project root directory
+git_dir=$(git rev-parse --show-toplevel)
+cd $git_dir 1>runTests.log
+
+# add logger to the scope
+source $(pwd)/tools/scripts/utils/logger.sh
+
+CLEAN_BUILD=0
+
+while getopts 'f' flag;
+do
+    case "${flag}" in
+        f)
+            CLEAN_BUILD=1
+        ;;
+        ?)
+            echo "script usage: $(basename \$0) [-f]" >&2
+            exit 1
+        ;;
+    esac
+done
+
+CMAKE_TOOLCHAIN_FILE_DIR=$(pwd)/tools/vcpkg/scripts/buildsystems
+CMAKE_BINARY_DIR=build/CI/
+CMAKE_BUILD_TYPE=Release
+
+if [[ $CLEAN_BUILD != 0 ]]; then
+    log_warn "Removing previous CI build ..."
+    rm -rf $CMAKE_BINARY_DIR
+    log_warn "Removing previous CI build -- DONE"
+fi
+
+# ============================================================================
+# CMake Generating, building and testing stages
+# ============================================================================
+
+# ----------------------------------------------------------------------------
+# Generating
+# ----------------------------------------------------------------------------
+
+CMAKE_CTestTestfile=$CMAKE_BINARY_DIR/CTestTestfile.cmake
+if [[ ! -f "$CMAKE_CTestTestfile" ]]; then
+    log_info "Generating build system..."
+    cmake -S . -B $CMAKE_BINARY_DIR -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_FILE_DIR/vcpkg.cmake -DENABLE_PERFORMANCE_TESTS=FALSE -DBENCHMARK_ENABLE_GTEST_TESTS=FALSE -DBENCHMARK_ENABLE_TESTING=FALSE -DCMAKE_CROSSCOMPILING=1 -DRUN_HAVE_STD_REGEX=0 -DRUN_HAVE_POSIX_REGEX=0 -DCMAKE_CXX_COMPILER="clang++"
+    if [[ "$?" != 0 ]]; then
+        log_error "Generating - FAILED"
+        exit 1
+    else
+        log_info "Generating build system -- done"
+    fi
+fi
+
+# -----------------------------------------------------------------------------
+# Building
+# -----------------------------------------------------------------------------
+log_info "Building..."
+cmake --build $CMAKE_BINARY_DIR --config $CMAKE_BUILD_TYPE
+if [[ "$?" != 0 ]]; then
+    log_error "Building - FAILED"
+    exit 1
+fi
+
+# -----------------------------------------------------------------------------
+# Testing
+# -----------------------------------------------------------------------------
+
+log_info "Testing..."
+ctest --test-dir $CMAKE_BINARY_DIR -C $CMAKE_BUILD_TYPE --output-on-failure --timeout 10
+if [[ "$?" != 0 ]]; then
+    log_error "Testing - FAILED"
+    exit 1
+else
+    log_info "Testing - DONE"
+fi
+
+# =============================================================================
+# Clean up
+# =============================================================================
+
+# return to previous cwd
+cd - 1> runTests.log
+rm runTests.log
--- scripts/git/utils/testLogger.sh
+++ scripts/git/utils/testLogger.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-source $(pwd)/tools/scripts/utils/logger.sh
+git_dir=$(git rev-parse --show-toplevel)
+cd $git_dir 1>testLogger.log
+source $(pwd)/tools/scripts/utils/logger.sh 1>testLogger.log
+cd - 1> testLogger.log
+rm testLogger.log
 
 while getopts 'v' flag;
 do
@@ -8,13 +12,15 @@ do
         v)
             echo "VERBOSE"
             CMAKE_CURRENT_LOG_LEVEL=CMAKE_LOG_LEVEL_TRACE
-            ;;
+        ;;
         ?)
             echo "script usage: $(basename \$0) [-v]" >&2
             exit 1
-            ;;
+        ;;
     esac
 done
+
+
 
 log_trace "Testing LOG TRACE"
 log_debug "Testing LOG DEBUG"
